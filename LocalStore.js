bdd6a95a9fea0be218b9f9674514270ce6a14d66
Fabian Vogelsteller <fabian.vogelsteller@tunedin.de>
2014-06-16 10:34:09 +0200

    add reactive behaviour

--- LocalStore.js
+++ LocalStore.js
@@ -1,17 +1,83 @@
 
-// the localstorage wrapper class
-LocalStore = {
-	set: function(key, value, callback){
+/**
+The LocalStore singleton.
 
-		// use chrome storage
+@class TemplateStore
+@constructor
+**/
+LocalStore = {
+    /**
+    This object stores all keys and their values.
+
+    @property keys
+    @type Object
+    @default {}
+    @example
+
+        {
+            name->myProperty: "myValue",
+            ...
+        }
+
+    **/
+    keys: {},
+
+
+    /**
+    Keeps the dependencies for the keys in the store.
+
+    @property deps
+    @type Object
+    @default {}
+    @example
+
+        {
+            name->myProperty: new Deps.Dependency,
+            ...
+        }
+
+    **/
+    deps: {},
+
+    // METHODS
+
+    // PRIVATE
+    /**
+    Creates at least ones a `Deps.Dependency` object to a key.
+
+    @method _ensureDeps
+    @private
+    @param {String} key     the name of the key to add a dependecy tracker to
+    @return undefined
+    **/
+    _ensureDeps: function (key) {
+        if (!this.deps[key]){
+            this.deps[key] = new Deps.Dependency;
+        }
+    },
+	set: function(key, value, options, callback){
+
+        this._ensureDeps(key);
+
+		// USE CHROME STORAGE
 		if(chrome && chrome.storage) {
 			var item = {};
 			item[key] = value;
 
 			// set
-			chrome.storage.local.set(item, callback);
+			chrome.storage.local.set(item, function(){
 
-		// use localstorage
+				// re-run reactive functions
+				if(!options || options.reactive !== false)
+	                this.deps[key].changed();
+
+	            // run callbacks
+				if(_.isFunction(callback))
+					callback();
+			});
+
+
+		// USE LOCALSTORAGE
 		} else {
 			// stringify
 			if(_.isObject(value))
@@ -20,12 +86,24 @@ LocalStore = {
 			// set
 			localStorage.setItem(key, value);
 
+			// re-run reactive functions
+			if(!options || options.reactive !== false)
+                this.deps[key].changed();
+
+			// run callbacks
 			if(_.isFunction(callback))
 				callback();
 		}
-
 	},
-	get: function(key, callback){
+	get: function(key, options, callback){
+
+        this._ensureDeps(key);
+
+
+        // DEPEND REACTIVE FUNCTIONS
+		if(!options || options.reactive !== false)
+            this.deps[key].depend();
+
 
 		// use chrome storage
 		if(chrome && chrome.storage) {
@@ -33,7 +111,8 @@ LocalStore = {
 			// get
 			chrome.storage.local.get(key, callback);
 
-		// use localstorage
+
+		// USE LOCALSTORAGE
 		} else {
 			// get
 			var value = localStorage.getItem(key),
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Installation
 Description
 -----------
 
-This is a simple wrapper for localstorage, which will use chrome.storage.local if its used in a chrome packaged app.
+This is a **reactive** wrapper for localstorage, which will use chrome.storage.local if its used in a chrome packaged app.
 
 To set an item just use:
 
@@ -20,3 +20,21 @@ To set an item just use:
 To get:
 
     var value = LocalStore.get('myKey');
+
+
+Additionally you can prevent reactive behavior, by passing `{reactive: false}`:
+
+
+    LocalStore.set('myKey', 'myString', {reactive: false});
+
+When getting:
+
+    var value = LocalStore.get('myKey', {reactive: false});
+
+
+As last parameter, you can also run a callback after the data was set, or get
+
+    LocalStore.set('myKey', {my: 'Object'}, false, function(){
+    	// do x	
+	});
+
